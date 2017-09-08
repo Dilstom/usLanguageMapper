f7cfd76c60749c99a7d5d17554d8bef8ab17cd03
CS Ahn <information@feyn.co>
2017-09-08 04:19:43 -0700

    Done

--- assets/javascript/uStates.js
+++ assets/javascript/uStates.js
@@ -58,7 +58,7 @@
 		function mouseOver(d){
 			d3.select("#tooltip").transition().duration(200).style("opacity", .9);      
 			
-			d3.select("#tooltip").html(toolTip(d.n, data[d.id]))  
+			d3.select("#tooltip").html(toolTip(d.n, data[d.n]))  
 				.style("left", (d3.event.pageX) + "px")     
 				.style("top", (d3.event.pageY - 28) + "px");
 		}
@@ -69,9 +69,9 @@
 		
 		d3.select(id).selectAll(".state")
 			.data(uStatePaths).enter().append("path").attr("class","state").attr("d",function(d){ return d.d;})
-			.style("fill",function(d){ return data[d.id].color; })
+			.style("fill",function(d){ return data[d.n.color]})
 			.on("mouseover", mouseOver).on("mouseout", mouseOut);
 	}
-	this.uStates=uStates;
-})();
+	window.uStates=uStates;
+}); 
 
